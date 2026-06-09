d6eaec062e5c488f418e1e43e5885ce2dd8eaf68
vanshika4139 <vanshika3926@gmail.com>
2026-06-09 13:04:05 +0530

    fix email port

--- backend/routes/loginRoutes.js
+++ backend/routes/loginRoutes.js
@@ -17,7 +17,9 @@ const otpStore = {};
 
 // Nodemailer transporter
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
